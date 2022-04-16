386fb5602a882039fd108c912b019ce66bf7ca91
Phungcode <Tkakapanter007@gmail.com>
2022-04-16 21:57:07 +0700

    update

--- Bài tập cộng điểm/baitap2_61136475/app/src/main/java/cntt/ntu/baitap2_61136475/MainActivity.java
+++ Bài tập cộng điểm/baitap2_61136475/app/src/main/java/cntt/ntu/baitap2_61136475/MainActivity.java
@@ -3,12 +3,31 @@ package cntt.ntu.baitap2_61136475;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit ;
+    Button btn;
+    RadioButton click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = (EditText) findViewById(R.id.nhap);
+        btn = (Button) findViewById(R.id.xuat);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dl = edit.getText().toString();
+            }
+        });
+
     }
 }
