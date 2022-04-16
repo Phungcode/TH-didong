26674a6720a432f33d11ddf374e381e47810a634
Phungcode <Tkakapanter007@gmail.com>
2022-04-16 23:06:56 +0700

    update

---
+++ BaiTapCongDiem/baitap2_61136475/app/build/generated/source/buildConfig/debug/cntt/ntu/baitap2_61136475/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cntt.ntu.baitap2_61136475;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cntt.ntu.baitap2_61136475";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
